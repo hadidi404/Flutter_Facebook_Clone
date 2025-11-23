4da1bd26fe6759c72b4a61f3db7ef600effef1b2
hadidi404 <raysonbarnachia@gmail.com>
2025-11-23 18:43:57 +0800

    Implement Facebook-style navigation bar below AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
-  const MyApp ({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -12,13 +14,19 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('facebook', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28, color: Colors.blue)),
+          title: const Text(
+            'facebook',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 28,
+              color: Colors.blue,
+            ),
+          ),
           actions: [
             IconButton(
               icon: const Icon(Icons.add),
               iconSize: 30,
               onPressed: () {},
-              
             ),
             const SizedBox(width: 8),
             IconButton(
@@ -31,7 +39,36 @@ class MyApp extends StatelessWidget {
               icon: const FaIcon(FontAwesomeIcons.facebookMessenger),
               onPressed: () {},
             ),
-            const SizedBox(width: 8)
+            const SizedBox(width: 8),
+          ],
+        ),
+        body: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(icon: const Icon(Icons.home), onPressed: () {}),
+                  IconButton(
+                    icon: const Icon(Icons.ondemand_video),
+                    onPressed: () {},
+                  ),
+                  IconButton(icon: const Icon(Icons.people), onPressed: () {}),
+                  IconButton(
+                    icon: const Icon(Icons.storefront),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.notifications),
+                    onPressed: () {},
+                  ),
+                  IconButton(icon: const Icon(Icons.menu), onPressed: () {}),
+                ],
+              ),
+            ),
+            // Add your main content here
           ],
         ),
       ),
